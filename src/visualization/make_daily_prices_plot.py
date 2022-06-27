c2b0b480ea017d2736757d6a8cb30e4cd3c69cd9
fa <fa@sempli.co>
2022-06-27 13:47:37 -0500

    ok

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -1,3 +1,10 @@
+def get_project_root():
+
+    from pathlib import Path
+
+    return Path(__file__).parent.parent.parent
+
+
 def make_daily_prices_plot():
     """Crea un grafico de lines que representa los precios promedios diarios.
 
@@ -7,10 +14,21 @@ def make_daily_prices_plot():
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    # raise NotImplementedError("Implementar esta función")
+
+    import pandas as pd
+
+    project_path = str(get_project_root())
+    path = project_path + "/data_lake/business/precios-diarios.csv"
+
+    df = pd.read_csv(path)
+    plot = df.plot(kind="line", x="fecha", y="precio", figsize=[40, 20])
+    fig = plot.get_figure()
+    fig.savefig(project_path + "/data_lake/business/reports/figures/daily_prices.png")
 
 
 if __name__ == "__main__":
     import doctest
 
+    make_daily_prices_plot()
     doctest.testmod()
